474ee11a40bd94b0cd24eb61fe554d85a41593b9
Adam Biagianti <adam.biagianti@gmail.com>
2013-07-26 10:12:26 -0700

    adds app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Caveat::Application.config.secret_token = 'a18b4786e108b7d0a3fc24d25ce24ec6f97b8b8252f4cf94e81e37418e28d3c32899b274b89c7fd37c2878b09c710b6d53e87b8ecc44b66cab1241958244f433'
