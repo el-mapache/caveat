f780b358deb95beb3e99bb88e29e56b688c3d0ee
Adam Biagianti <adam.biagianti@gmail.com>
2013-12-05 21:14:07 -0800

    bypasses reverse geocode

--- app/controllers/api/v1/businesses_controller.rb
+++ app/controllers/api/v1/businesses_controller.rb
@@ -3,15 +3,15 @@ class API::V1::BusinessesController < ApplicationController
     if params[:name] == "all"
       render json: Business.select("businesses.name").find(:all) and return
     end
-      
-    cache = $redis.get(params[:address])
-    
-    if cache
-      @businesses = JSON.parse(cache)
-    else
+
+    #cache = $redis.get(params[:address])
+
+    #if cache
+     # @businesses = JSON.parse(cache)
+   # else
       @businesses = API::V1::BusinessPresenter.present(Business.all_with_associations([params[:lat], params[:lng]]))
-      $redis.setex(params[:address], 172800, @businesses.to_json)
-    end
+     # $redis.setex(params[:address], 172800, @businesses.to_json)
+    #end
 
     render json: @businesses
   end
