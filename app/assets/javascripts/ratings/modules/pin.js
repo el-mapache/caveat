42f1497f2a182d02e33bef26aa4188cc7db0d2dc
Adam Biagianti <adam.biagianti@gmail.com>
2014-03-20 19:48:40 -0700

    not working and i dont know why

--- app/assets/javascripts/ratings/modules/pin.js
+++ app/assets/javascripts/ratings/modules/pin.js
@@ -1,4 +1,4 @@
-angular.module("Pin",['InfoWindow']).factory("Pin", function($compile, InfoWindow) {
+angular.module("Pin",['InfoWindow']).factory("Pin", function(InfoWindow) {
   function Pin(opts, content, index) {
     google.maps.Marker.apply(this, arguments);
     this.active = false;
@@ -6,21 +6,21 @@ angular.module("Pin",['InfoWindow']).factory("Pin", function($compile, InfoWindo
     this.data = content || {};
     this.index = index;
   }
-  
+
   Pin.prototype = new google.maps.Marker();
-  
+
   Pin.prototype.toggleInfoWindow = function(template) {
-    if (this.active) { 
+    if (this.active) {
       this.infoWindow.remove();
       this.active = false;
       this.infoWindow = null;
     } else {
-      var infoWindow = new InfoWindow({latlng: this.getPosition(), map: this.map, content: template});      
+      var infoWindow = new InfoWindow({latlng: this.getPosition(), map: this.map, content: template});
       this.infoWindow = infoWindow;
       this.active = true;
     }
   };
-  
+
   Pin.prototype.remove = function() {
     google.maps.event.clearInstanceListeners(this);
     this.setMap(null);
@@ -33,6 +33,7 @@ angular.module("Pin",['InfoWindow']).factory("Pin", function($compile, InfoWindo
     this.infoWindow.remove();
     this.infoWindow = null;
   };
-  
+
   return Pin;
 });
+
